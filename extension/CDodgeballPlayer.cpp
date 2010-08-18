c18a6eea5886914e4fa8f3e3a53288756e250084
Asher Baker <asherkin@limetech.org>
2010-08-18 17:44:22 +0700

    Fixed crash with changing class to spy. DERP.

--- extension/CDodgeballPlayer.cpp
+++ extension/CDodgeballPlayer.cpp
@@ -25,7 +25,7 @@ void CDodgeballPlayer::Spawn(void)
 
 CBaseEntity *CDodgeballPlayer::GiveNamedItem(char const *szName, int iSubType, CScriptCreatedItem *item, bool bUnknown)
 {
-	if (!DodgeballEnabled.GetBool())
+	if (!DodgeballEnabled.GetBool() || !item)
 	{
 		return BaseClass::GiveNamedItem(szName, iSubType, item, bUnknown);
 	} else if (GetPlayerClass() == PLAYERCLASS_PYRO && !(item->m_iItemDefinitionIndex == 21 || item->m_iItemDefinitionIndex == 40)) {
