81c74b286914ff549489256569edb0c4985136e5
Asher Baker <asherkin@limetech.org>
2012-02-03 17:17:35 +0000

    If I don't commit this I'll lose it.

--- extension/CTFDBFlameThrower.cpp
+++ extension/CTFDBFlameThrower.cpp
@@ -6,14 +6,26 @@
 LINK_ENTITY_TO_CLASS(CTFFlameThrower, CTFDBFlameThrower);
 
 ConVar DeflectPlayers("sm_dodgeball_deflect_players", "1", FCVAR_NONE, "", true, 0.0, true, 1.0);
+ConVar DeflectPlayersDamage("sm_dodgeball_deflect_players_damage", "0", FCVAR_NONE, "", true, 0.0, false, 0.0);
 
 bool CTFDBFlameThrower::DeflectPlayer(CBaseEntity *pTarget, CBaseEntity *pOwner, Vector &vecForward, Vector &vecCenter, Vector &vecSize)
 {
-	if (!DodgeballEnabled.GetBool() || DeflectPlayers.GetBool() || (pTarget && pOwner && (CEntity::Instance(pTarget)->GetTeamNumber() == CEntity::Instance(pOwner)->GetTeamNumber())))
+	if (!DodgeballEnabled.GetBool() || (pTarget && pOwner && (CEntity::Instance(pTarget)->GetTeamNumber() == CEntity::Instance(pOwner)->GetTeamNumber())))
 	{
 		return BaseClass::DeflectPlayer(pTarget, pOwner, vecForward, vecCenter, vecSize);
 	} else {
-		return false;
+		if (DeflectPlayersDamage.GetInt())
+		{
+			META_CONPRINTF("Implement me, please :(\n");
+			//CEntity::Instance(pOwner)->OnTakeDamage(NULL);
+		}
+
+		if (DeflectPlayers.GetBool())
+		{
+			return BaseClass::DeflectPlayer(pTarget, pOwner, vecForward, vecCenter, vecSize);
+		} else {
+			return false;
+		}
 	}
 }
 
